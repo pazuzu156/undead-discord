a6319fe59efeae4e313d1ccf8a9e84072fe42452
Kaleb Klein <klein.jae@gmail.com>
2018-05-11 16:27:24 -0400

    Changed syntax colors to base16 and updated build process
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,11 @@
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-file-append');
+    grunt.loadNpmTasks('grunt-sass');
+
+    var pkg = require('./package.json');
 
     grunt.initConfig({
         sass: {
@@ -18,9 +21,25 @@ module.exports = function (grunt) {
                     {
                         expand: true,
                         cwd: 'dist',
-                        src: ['*.css', '!*.min.css'],
+                        src: ['*.css', '!*.min.css', '!*.theme.css'],
                         dest: 'dist',
                         ext: '.min.css'
+                    }
+                ]
+            }
+        },
+        file_append: {
+            default_options: {
+                files: [
+                    {
+                        append: '',
+                        prepend: '//META\
+{"name":"Undead Discord",\
+"description":"'+ pkg.description + '"\
+"author":"'+ pkg.author.name + '"\
+"version":"'+ pkg.version + '"}*//\n\n',
+                        input: 'dist/undead-discord.min.css',
+                        output: 'dist/undead-discord.min.theme.css'
                     }
                 ]
             }
@@ -38,6 +57,6 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('default', ['sass', 'cssmin']);
-    grunt.registerTask('run', ['watch']);
+    grunt.registerTask('build', ['clean', 'sass', 'cssmin', 'file_append']);
+    grunt.registerTask('default', ['watch']);
 };
